257cd07f36e00b5f8f238f9e2858ca624fc2827a
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2022-03-29 20:48:38 -0300

    Requisitos 5 e 6

--- script.js
+++ script.js
@@ -7,11 +7,25 @@ function printText() {
 document.getElementById('text-input').addEventListener('input', printText);
 
 // Adiciona a imagem ao meme ao ser selecionada
-function printImg(event) {
+function printImg() {
   const img = URL.createObjectURL(this.files[0]);
-  document.getElementById('meme-image-container').style.backgroundImage = `url(${img})`
+  document.getElementById('meme-image-container').style.backgroundImage = `url(${img})`;
 }
 
 document.getElementById('meme-insert').addEventListener('input', printImg);
 
+// Adiciona estilo à borda do meme
+function customer() {
+  const tag = document.getElementById('meme-image-container');
+  if (this.id === 'fire') {
+    tag.style.border = '3px dashed red';
+  } else if (this.id === 'water') {
+    tag.style.border = '5px double blue';
+  } else {
+    tag.style.border = '6px groove green';
+  }
+}
 
+for (const btn of document.getElementsByTagName('button')) {
+  btn.addEventListener('click', customer);
+}
