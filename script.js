d56090fc4b33ca60ddf0c7c59f6564dc519c9d98
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2022-03-30 14:17:24 -0300

    Simplifica o código

--- script.js
+++ script.js
@@ -1,51 +1,28 @@
+const memeId = document.getElementById('meme-image');
+
 // Adiciona o texto ao meme logo ao ser digitado
-function printText() {
+document.getElementById('text-input').addEventListener('input', () => {
   const text = document.getElementById('text-input').value;
   document.getElementById('meme-text').innerText = text;
-}
-
-document.getElementById('text-input').addEventListener('input', printText);
+});
 
 // Adiciona a imagem ao meme ao ser selecionada
-const memeId = 'meme-image';
-function printImg() {
-  const img = URL.createObjectURL(this.files[0]);
-  document.getElementById(memeId).style.backgroundImage = `url(${img})`;
-}
-
-document.getElementById('meme-insert').addEventListener('input', printImg);
+document.getElementById('meme-insert').addEventListener('input', (element) => {
+  const img = URL.createObjectURL(element.target.files[0]);
+  memeId.style.backgroundImage = `url(${img})`;
+});
 
 // Adiciona estilo à borda do meme
-function customer() {
-  const tag = document.getElementById('meme-image-container');
-  if (this.id === 'fire') {
-    tag.style.border = '3px dashed red';
-  } else if (this.id === 'water') {
-    tag.style.border = '5px double blue';
-  } else {
-    tag.style.border = '6px groove green';
-  }
-}
-
 [...document.getElementsByTagName('button')].forEach((element) => {
-  element.addEventListener('click', customer);
+  element.addEventListener('click', () => {
+    const tag = document.getElementById('meme-image-container');
+    tag.style.border = element.value;
+  });
 });
 
 // Adiciona imagens prontas ao fundo do meme
-function setImg() {
-  const tag = document.getElementById(memeId);
-  console.log(this.src);
-  if (this.id === 'meme-1') {
-    tag.src = this.src;
-  } else if (this.id === 'meme-2') {
-    tag.src = this.src;
-  } else if (this.id === 'meme-3') {
-    tag.src = this.src;
-  } else {
-    tag.src = this.src;
-  }
-}
-
 [...document.getElementsByTagName('img')].forEach((element) => {
-  element.addEventListener('click', setImg);
+  element.addEventListener('click', () => {
+    memeId.src = element.src;
+  });
 });
